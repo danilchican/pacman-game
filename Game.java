083d703338812ef2721e5237c23be73ebc8f380e
Vladislav Danilchik <danilchican@mail.ru>
2016-02-28 15:00:38 +0300

    First init

---
+++ Game.java
@@ -0,0 +1,27 @@
+package com.danilchican.pacman;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Game extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("Pacman");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
