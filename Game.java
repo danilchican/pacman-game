73636624604373d385ea927859b36823a8b776b6
Vladislav Danilchik <danilchican@mail.ru>
2016-03-17 22:15:00 +0300

    Added comments
    Change-Id: Ie9cf18e955eb288fe39253657a1ad37c338ee693

--- Game.java
+++ Game.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+/////////////////////////////////////////////
 public class Game extends Application {
 
 	@Override
